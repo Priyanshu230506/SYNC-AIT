75625b536cee36c2fd51a0ead4111d68135e0d66
Vishal Goswami <87699253+MyTricks-code@users.noreply.github.com>
2025-12-07 22:31:08 +0530

    tailwind fixed

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='mt-10 bg-red-600'>Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla expedita consectetur, porro similique odit necessitatibus saepe est enim, magni animi voluptatem quis. Repudiandae doloribus voluptatum libero tempora minus, at, eum distinctio ullam dicta assumenda ut ducimus corporis omnis harum.</div>
+    <div className='h-screen bg-red-600'>Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla expedita consectetur, porro similique odit necessitatibus saepe est enim, magni animi voluptatem quis. Repudiandae doloribus voluptatum libero tempora minus, at, eum distinctio ullam dicta assumenda ut ducimus corporis omnis harum.</div>
   )
 }
 
